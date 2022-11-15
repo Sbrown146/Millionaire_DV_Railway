c54a45f7f4b987e3408e6c91039c5f7c04428cef
Scott Brown <scott_brown_jp@yahoo.com>
2022-11-15 18:12:29 -0500

    Trying to get configCred to work

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 import pandas as pd
 import numpy as np
 
-from .configCred import postgres_route
+# from .configCred import postgres_route
 # from configCred import postgres_route
+
+from config import ConfigCred
 
 
 import sqlalchemy
