56c09cfa18da6791cdaf48a5a47131282ebb9554
Scott Brown <scott_brown_jp@yahoo.com>
2022-11-16 15:23:45 -0500

    Trying to get database_url to be read in properly

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import numpy as np
 
 # from .configCred import postgres_route
 # from configCred import postgres_route
-from .config.configCred import postgres
+# from .config.configCred import postgres
+from .config.import configCred import postgres_route
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
