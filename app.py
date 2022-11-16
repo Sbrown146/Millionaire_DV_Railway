9a373d995564aca1a9dcfe857682215abd25c8cb
Scott Brown <scott_brown_jp@yahoo.com>
2022-11-16 17:21:48 -0500

    Trying to get database_url to be read in properly

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import numpy as np
 import sys
 sys.path.append(r"D:\Old_C_Drive_Current_Repos\Millionaire_Railway\config")
 
-# from .configCred import postgres_route
+from .configCred import postgres_route
 # from configCred import postgres_route
 # from .config.configCred import postgres_route
 # from ..config import configCred
-from . import configCred
 
 
 import sqlalchemy
